0f9bdf12c49644888c593c0dc2deb07510bd583b
Emile-Aquila <tmk8oakan6130@gmail.com>
2023-09-04 21:44:51 +0900

    set MCMD3 (z) params, and verified

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -174,23 +174,24 @@ int main(void)
         mcmd_handlers[0].device.node_type = NODE_MCMD3;
         mcmd_handlers[0].device.node_id = 1;
         mcmd_handlers[0].device.device_num = 0;
-        mcmd_handlers[0].ctrl_param.ctrl_type = MCMD_CTRL_DUTY;
+        mcmd_handlers[0].ctrl_param.ctrl_type = MCMD_CTRL_POS;
         mcmd_handlers[0].ctrl_param.feedback = MCMD_FB_ENABLE;
         mcmd_handlers[0].fb_type = MCMD_FB_POS;
-        mcmd_handlers[0].enc_dir = MCMD_DIR_FW;  // TODO: ?
-        mcmd_handlers[0].rot_dir = MCMD_DIR_FW;  // TODO: ?
+        mcmd_handlers[0].limit_sw_type = LIMIT_SW_NO;
+        mcmd_handlers[0].enc_dir = MCMD_DIR_FW;
+        mcmd_handlers[0].rot_dir = MCMD_DIR_FW;
         mcmd_handlers[0].calib = CALIBRATION_ENABLE;
         mcmd_handlers[0].ctrl_param.gravity_compensation = GRAVITY_COMPENSATION_DISABLE;
-        mcmd_handlers[0].calib_duty = -0.2f;
-        mcmd_handlers[0].quant_per_unit = 90.0f / 1024.0f;  // TODO: 分解能が不明
+        mcmd_handlers[0].calib_duty = -0.5f;
+        mcmd_handlers[0].quant_per_unit = 90.0f / 1024.0f;
         mcmd_handlers[0].ctrl_param.accel_limit = ACCEL_LIMIT_ENABLE;
-        mcmd_handlers[0].ctrl_param.accel_limit_size = 10.0f;
-        mcmd_handlers[0].ctrl_param.PID_param.kp = 0.1f;
+        mcmd_handlers[0].ctrl_param.accel_limit_size = 5.0f;
+        mcmd_handlers[0].ctrl_param.PID_param.kp = 0.12f;
 
         MCMD_init(&mcmd_handlers[0]);
         MCMD_Calib(&mcmd_handlers[0]);  // キャリブレーションを行う
         HAL_Delay(2000);
-        MCMD_SetTarget(&mcmd_handlers[0], 0.0f);  // 目標値(0.0)を設定
+        MCMD_SetTarget(&mcmd_handlers[0], 100.0f);  // 目標値(0.0)を設定
         // TODO: 稼働限界は226mm
         MCMD_Control_Enable(&mcmd_handlers[0]);  // 制御開始
     }
